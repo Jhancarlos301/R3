f4adb285221c3094499d15ec6fb96c7f3c718e31
Jhancarlos301 <112730654+Jhancarlos301@users.noreply.github.com>
2022-11-10 20:31:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -29,13 +29,13 @@ jwplayer("container").setup({
     description: "El amor de Tessa y Hardin nunca ha sido fácil. Mientras él permanece en Londres después de la boda de su madre y se hunde cada vez más en su propia oscuridad, ella regresa a Seattle. Tessa es la única capaz de entenderle y calmarle… él la necesita, pero ella ya no es la chica buena y dulce que era cuando llegó a la universidad. Deberá plantearse si lo que debe hacer ahora es salvar a Hardin y su relación con él, o si ha llegado el momento de pensar solo en ella. Si quieren que su amor sobreviva, primero tendrán que trabajar en sí mismos. ¿Pero será su destino seguir estando juntos?",
     image: "https://www.themoviedb.org/t/p/original/rwgmDkIEv8VjAsWx25ottJrFvpO.jpg",
     sources: [{
-    file: "https://reproductor.appsmartt.com/mp4/api.php?v=w84xpsn466pywxq.mp4",
+    file: "https://archive.org/download/pelis-n/Ver%20Amn%C3%A9sico.mp4",
       label: '4K',
       'type': 'mp4',
       primary: 'html5',
 
     },{
-    file: "https://reproductor.appsmartt.com/mp4/api.php?v=w84xpsn466pywxq.mp4",
+    file: "https://archive.org/download/pelis-n/Ver%20Amn%C3%A9sico.mp4",
       label: '1080p',
       'type': 'mp4',
       primary: 'html5',
