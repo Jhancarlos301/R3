6fd8e50f27b4578ada74e519079217a7ee086873
Jhancarlos301 <112730654+Jhancarlos301@users.noreply.github.com>
2022-11-30 00:07:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ jwplayer("container").setup({
     description: "El amor de Tessa y Hardin nunca ha sido fácil. Mientras él permanece en Londres después de la boda de su madre y se hunde cada vez más en su propia oscuridad, ella regresa a Seattle. Tessa es la única capaz de entenderle y calmarle… él la necesita, pero ella ya no es la chica buena y dulce que era cuando llegó a la universidad. Deberá plantearse si lo que debe hacer ahora es salvar a Hardin y su relación con él, o si ha llegado el momento de pensar solo en ella. Si quieren que su amor sobreviva, primero tendrán que trabajar en sí mismos. ¿Pero será su destino seguir estando juntos?",
     image: "https://www.themoviedb.org/t/p/original/rwgmDkIEv8VjAsWx25ottJrFvpO.jpg",
     sources: [{
-    file: "https://archive.org/download/pelis-n/Ver%20Amn%C3%A9sico.mp4",
+    file: "https://lahousetrialgameplayonlinetomax.xyz/repro2.php?id=bHJMRW1oTVhldDJJZnZnUE10V3NBQmx5b2ZqemljRFk1RmUybGphTUI2ZFhTWTNzZkUwOXAza0NTSktMWW9vQ1pWSXViY2JaOEdPazE4bjBVS2NMUXhlRWUwWWhxU0ZjdGNIT0dBWFI5U2VEU3luUzgzQUlNL3NMV3l2WjB2YmJMSVI2WUhCRW9kUnROWlZOVkxIdEV6YktNbEhEZEJjMjFYdVFBelRWOHlyQVQzdUpad3hGbHVBKzBVN3RBdUpjaTBTclFUNVhXdmVvNWpGSFk1NVNXaERscmVKZDA4U05XYlZRUEwrdFRYd2lReFowYy9iR2o5bkpmWVJtYm5oUzlYN3lmODFoc3dlb21jNkZMUnRhbGZESzFQL0N2YUtjZE9MTTJyUkUwV2VKR1hoZDlucWhSTWJ2Ymx3bitZR0tsYWhydC9QS2NmbXJKTjhHVHlWZGZ3PT0&type=1",
       label: '4K',
       'type': 'mp4',
       primary: 'html5',
